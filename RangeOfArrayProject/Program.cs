de8fd8b43b88158381058d59593d5d56a7bd01a1
Poli_tn62@student.itstep.kz <Poli_tn62@student.itstep.kz>
2020-12-25 21:30:23 +0600

    Добавьте файлы проекта.

---
+++ RangeOfArrayProject/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+
+namespace RangeOfArrayProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = -5, max = 15;
+            RangeOfArray mas = new RangeOfArray
+            {
+                Subscript = min,
+                Superscript = max
+            };
+
+            for (int i = min, j = 0; i <= max; i++, j++)
+            {
+                mas[i] = j;
+            }
+            for (int i = min; i <= max; i++)
+            {
+                Console.WriteLine($"Элемент {i} = {mas[i]}");
+            }
+            mas[max + 1] = 10;
+            mas[min - 1] = 10;
+            Console.WriteLine(mas[max + 1]);
+            Console.WriteLine(mas[min - 1]);
+        }
+    }
+}
---
+++ RangeOfArrayProject/RangeOfArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RangeOfArrayProject
+{
+    class RangeOfArray
+    {
+        public int Superscript { get; set; } = 0; //верхний индекс
+        public int Subscript { get; set; } = 0;   //нижний индекс
+
+        private int[] array = new int[200];
+        public int this[int index]
+        {
+            get
+            {
+                if (index >= Subscript && index <= Superscript)
+                {
+                    return array[index - Subscript];
+                }
+                return int.MaxValue;
+            }
+            set
+            {
+                if (index >= Subscript && index <= Superscript)
+                    array[index - Subscript] = value;
+            }
+        }
+
+    }
+}
